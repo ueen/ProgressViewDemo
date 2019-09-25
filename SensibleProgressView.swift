1afc8b6ca4aa4ecad05d38e366d7ad068d481bae
ueen <ueli.sarnighausen@online.de>
2019-09-25 21:17:01 +0200

    Update SensibleProgressView.swift

--- SensibleProgressView.swift
+++ SensibleProgressView.swift
@@ -14,6 +14,21 @@ class SensibleProgressView: UIView {
     private var progressBackgroundView: UIView!
     private var progressContentView: UIView!
     
+    @IBInspectable var trackColor: UIColor = UIColor.white {
+        didSet{
+            self.backgroundColor = trackColor
+            self.progressBackgroundView.backgroundColor = trackColor
+        }}
+    @IBInspectable var progressColor: UIColor = UIColor.blue {
+    didSet{
+        self.progressContentView.backgroundColor = progressColor
+        self.layer.borderColor = progressColor.cgColor
+    }}
+    @IBInspectable var borderThickness: CGFloat = 2 {
+    didSet{
+        self.layer.borderWidth = borderThickness
+    }}
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
@@ -27,31 +42,23 @@ class SensibleProgressView: UIView {
     }
     
     func build() {
-        let bgColor = UIColor.clear
-        let progressColor = UIColor.white
-        
-        backgroundColor = bgColor
-        layer.borderColor = progressColor.cgColor
-        layer.borderWidth = 2
+
         layer.cornerRadius = frame.size.height / 2
         layer.masksToBounds = true
         
         progressBackgroundView = UIView()
         progressBackgroundView.frame.size.height = frame.size.height
-        progressBackgroundView.backgroundColor = progressColor
         addSubview(progressBackgroundView)
         
         
         progressContentView = UIView()
-        progressContentView.backgroundColor = bgColor
         progressContentView.frame = progressBackgroundView.frame
         addSubview(progressContentView)
-        
     }
 
     
     /// The current progress value. Valid Range: 0.0 ~ 1.0
-    var progress: Float = 0.0 {
+    @IBInspectable  var progress: Float = 0.0 {
         didSet {
             let temp = max(0.0, progress)
             progress = min(temp, 1.0)
